128139fe0ddd000b4d0c5f0174be9d7de6c99b9a
Amani <amanialshmali7@gmail.com>
2024-10-10 08:25:51 +0400

    frist commit

---
+++ FootballGame/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FootballGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
