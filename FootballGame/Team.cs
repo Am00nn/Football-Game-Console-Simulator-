4b19928dcb34374f3438952860cd4a83cd2c3e10
Amani <amanialshmali7@gmail.com>
2024-10-10 08:30:08 +0400

    Adding Team class

---
+++ FootballGame/Team.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballGame
+{
+    public class Team
+    {
+        public string TeamName { get; set; }
+        players[] players { get; set; }
+
+        public Team(string teamName)
+        {
+            TeamName = teamName;
+            players = new players[11];
+
+        }
+    }
+}  
