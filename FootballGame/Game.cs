117138fb62132a15382c85e5144826529a30e0a8
Amani <amanialshmali7@gmail.com>
2024-10-10 08:37:26 +0400

    Adding game class

---
+++ FootballGame/Game.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballGame
+{
+    public class Game
+    {
+        public Team T1 { get; set; }
+        public Team T2 { get; set; }
+
+        public int T1Score = 0;
+        public int T2Score = 0;
+        public Random random;
+
+        public Game(Team T1, Team T2)
+        {
+
+            this.T1 = T1;
+            this.T2 = T2;
+            random = new Random();
+
+        }
+    }
+ }
