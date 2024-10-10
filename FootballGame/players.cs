1926cecb38b2f97020bb9ac0569633900e3c1bdb
Amani <amanialshmali7@gmail.com>
2024-10-10 08:28:04 +0400

    Adding players class

---
+++ FootballGame/players.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballGame
+{
+    public class players
+    {
+        public string PlayerName { get; set; }
+        public string PlayerPosition { get; set; }
+        public int PlayerPower { get; set; }
+
+        public players(string PlayerName, string PlayerPosition, int PlayerPower)
+        {
+
+
+            this.PlayerName = PlayerName;
+            this.PlayerPosition = PlayerPosition;
+            this.PlayerPower = PlayerPower;
+
+        }
+
+
+
+
+
+    }
+}
